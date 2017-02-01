3a649a51099e783b87ba33e9ff3c5a144396d1e5
shyling <i@shyling.com>
2017-02-01 12:14:27 +0800

    Add multithread

--- src/server.rs
+++ src/server.rs
@@ -2,8 +2,10 @@ use std::net::*;
 use request::Request;
 use response::ResponseState;
 use std::io::Error;
+use std::thread;
+use std::sync::{Arc, RwLock};
 
-type Handle = Box<Fn(&mut Request) -> ResponseState + Send + Sync>;
+type Handle = Box<Fn(&mut Request) -> ResponseState + Send + Sync + 'static>;
 
 pub struct Server {
     bind: &'static str,
@@ -22,29 +24,34 @@ impl Server {
         self.handles.push(h);
     }
 
-    pub fn add_handle<T: 'static + Fn(&mut Request) -> ResponseState + Send + Sync>(&mut self, h: T) {
+    pub fn add_handle<T>(&mut self, h: T)
+        where T: Fn(&mut Request) -> ResponseState + Send + Sync + 'static
+    {
         self.handles.push(Box::new(h));
     }
 
-    pub fn serve(&self) -> Result<(), Error> {
+    pub fn serve(self) -> Result<(), Error> {
         let listener = try!(TcpListener::bind(self.bind));
+        let handles = Arc::new(RwLock::new(self.handles));
         for stream in listener.incoming() {
-            match stream {
-                Ok(mut stream) => {
-                    let mut req = Request::from_tcp_stream(&stream);
-                    for handle in &self.handles {
-                        match handle(&mut req) {
-                            ResponseState::Show(res) => {
-                                try!(res.send(&mut stream));
-                                break;
-                                // try!(stream.shutdown(Shutdown::Both));
+            let handles = handles.clone();
+            thread::spawn(move || {
+                match stream {
+                    Ok(mut stream) => {
+                        let mut req = Request::from_tcp_stream(&stream);
+                        for handle in &*handles.read().unwrap() {
+                            match handle(&mut req) {
+                                ResponseState::Show(res) => {
+                                    res.send(&mut stream).unwrap();
+                                    // try!(stream.shutdown(Shutdown::Both));
+                                }
+                                ResponseState::Skip => (),
                             }
-                            ResponseState::Skip => (),
                         }
                     }
+                    Err(e) => panic!("{:?}", e),
                 }
-                Err(e) => panic!("{:?}", e),
-            }
+            });
         }
         Ok(())
     }
